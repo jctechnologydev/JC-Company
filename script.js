ebfa0e27411436c6962f79d65600933acf56c2c7
joeljonassi <joeljonassi@outlook.com>
2025-02-02 14:04:58 +0100

    solve style missing bug

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function toggleMenu() {
   menu.classList.toggle('active');
 }
 
+
+// Function to scale when mouse over Expertise element
+// Devoloped on 26/01/2025 
+document.querySelectorAll('.card').forEach((card) => {
+  card.addEventListener('mouseover', () => {
+    card.style.transform = 'scale(1.05)';
+    card.style.transition = 'transform 0.3s ease';
+  });
+  card.addEventListener('mouseout', () => {
+    card.style.transform = 'scale(1)';
+  });
+});
+
